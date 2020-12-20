adf9aac4646cdc39f89d8b6355bacbe0c873936b
GeorgijRom <gash20161@mail.ru>
2020-12-20 19:51:08 +0300

    test1and2

--- test/integration/users_flow_test.rb
+++ test/integration/users_flow_test.rb
@@ -1,18 +1,21 @@
-require 'test_helper'
+require './test/test_helper'
 
 class UsersFlowTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
   test "registrate and enter" do
-    get "/"
-    click_link "Registration"
-    assert_select "h1", "Welcome#index"
+    post "/users/registration", params: {name: "Anfisa", password: "123"}
+    assert_response :success
+    post "/users/login", params: {name: "Anfisa", password: "123"}
+    assert_response :success
+    get '/laba12/view.json', params: {happy_numbers_quantity: 7}
+    assert_response :success
   end
 
-  test "can't do math without login" do
-    get "/laba12/input"
-    assert_select '.menu a[href=?]', 'http://localhost:3000/users/registration',
-      { :count => 1, :text => 'Registration' }
-  end  
+ # test "can't do math without login" do
+ #   get "/laba12/input"
+ #   assert_select '.menu a[href=?]', 'http://localhost:3000/users/registration',
+ #     { :count => 1, :text => 'Registration' }
+ # end  
 end
